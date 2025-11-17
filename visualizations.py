2f136d422e7fd807b675e01e8d20ee5bcc25b55b
aka <fumf446@gmail.com>
2025-11-17 16:23:03 -0500

    visualizations file created

---
+++ visualizations.py
@@ -0,0 +1,11 @@
+def create_bar_chart(database, cuisines):
+    # bar chart of the number of recipes from a cuisine
+    pass
+
+def create_scatter(database, city):
+    # scatter of restutrant locations in a city
+    pass
+
+def create_pie_chart(database):
+    # weather condition across the city
+    pass
