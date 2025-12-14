68958415f17dead62fd321ac0b37f224ea516929
Kerrington Jackson <kerryjac@umich.edu>
2025-12-14 04:11:12 -0500

    finished adding and storing recipes setup as well as ingredients setup

--- spoonacular.py
+++ spoonacular.py
@@ -1,6 +1,6 @@
 import requests
 import sqlite3
-from api_key.txt import spoonacular_api
+from api_key import spoonacular_api
 
 
 limit = 25
@@ -13,32 +13,59 @@ def offset_recipes(): #pagination #avoid duplicates
     conn.close()
     return offset
     
+def get_cuisine(cur, cuisine):
+    cur.execute("INSERT OR IGNORE INTO cuisines (name) VALUES (?)", (cuisine,))
+    cur.execute("SELECT id FROM cuisines WHERE name=?", (cuisine,))
+    c_result = cur.fetchone()[0]
+    return c_result
 
-def spoonacular_recipes(api_key, cuisine, num):
-    base = 'https://api.spoonacular.com/recipes/complexSearch'
-    params = {
-        'apiKey': api_key, 
-        'addRecipeInformation': True,
-        'number': num
+
+def get_ingredient(cur, name):
+    cur.execute("INSERT OR IGNORE INTO ingredients (name) VALUES (?)", (name,))
+    cur.execute("SELECT id FROM ingredients WHERE name=?", (name,))
+    i_result = cur.fetchone()[0]
+    return i_result
+
+offset = offset_recipes()
+
+
+base = "https://api.spoonacular.com/recipes/complexSearch"
+params = {
+        "apiKey": spoonacular_api, 
+        "number": limit,
+        "offset": offset,
+        "addRecipeInformation": True
         }
     
 
-    response = requests.get(base, params=params)
-    info = response.json()
+response = requests.get(base, params=params)
+info = response.json()
 
+conn = sqlite3.connect("project.db")
+cur = conn.cursor()
 
-    results = info.get('results', [])
+results = info.get('results', [])
 
-    for recipe in results:
-        rec_id = recipe['id']
+for recipe in results:
+    cuisine = recipe["cuisines"][0] if recipe["cuisines"] else "Unknown"
+    cuisine_id = get_cuisine(cur, cuisine)
+    cur.execute("""
+        INSERT OR IGNORE INTO recipes
+        (spoon_id, title, cuisine_id, ready_time, source) VALUES (?, ?, ?, ?, ?)"""
+        , (recipe["id"], recipe["title"], cuisine_id, recipe["readyInMinutes"], recipe["sourceUrl"]))
+    cur.execute("SELECT id FROM recipes WHERE spoon_id=?", (recipe["id"],))
+    recipe_id = cur.fetchone()[0]
 
+    ext_ingredients = recipe.get("extendedIngredients", [])
+    if ext_ingredients:
+        for ing in ext_ingredients:
+            ing_id = get_ingredient(cur, ing["name"])
+            cur.execute("INSERT OR IGNORE INTO recipe_ingredients VALUES (?, ?)", (recipe_id, ing_id))
 
-        title = recipe.get('title')
-        cuisine = recipe.get('cuisine')
-        ready_time = recipe.get('readyInMinutes')
-        link = recipe.get('sourceURL')
+conn.commit()
+conn.close()
+print("Data from Spoonacular has been stored.")
 
-        ingredients = recipe.get('extendedIngredients')
         
 
 
